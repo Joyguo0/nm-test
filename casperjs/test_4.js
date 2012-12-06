a4ef1b3991bbf3582d6a5d8158d862a7567cff75
vitrum <vitrum.cn@gmail.com>
2012-12-06 17:45:48 +0800

    test4

---
+++ casperjs/test_4.js
@@ -0,0 +1,30 @@
+var casper = require('casper').create({
+    verbose: true,
+    logLevel: "debug"
+});
+
+var links = {
+    'http://www.glamour-sales.com.cn/': 0
+};
+
+casper.countLinks = function() {
+    return this.evaluate(function() {
+        return __utils__.findAll('a[href]').length;
+    });
+};
+
+casper.renderJSON = function(what) {
+    return this.echo(JSON.stringify(what, null, '  '));
+};
+
+casper.start();
+
+casper.each(Object.keys(links), function(casper, link) {
+    this.thenOpen(link, function() {
+        links[link] = this.countLinks();
+    });
+});
+
+casper.run(function() {
+    this.renderJSON(links).exit();
+});
